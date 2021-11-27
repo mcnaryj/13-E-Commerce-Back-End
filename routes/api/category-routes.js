350f4bd699e6b065ea4d1ab87dfa9fd0120f2e92
Jack McNary <jack.mcnary@gmail.com>
2021-11-27 15:52:02 -0700

    adding a try

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,6 +4,14 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Category }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
