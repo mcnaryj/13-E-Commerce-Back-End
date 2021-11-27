e7b7ff24f3e1c19259cb569e3008e0762838ed54
Jack McNary <jack.mcnary@gmail.com>
2021-11-27 15:57:03 -0700

    defining category columns

--- models/index.js
+++ models/index.js
@@ -5,9 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Products.belongsTo(, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
