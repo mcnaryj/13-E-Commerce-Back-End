e2e06ca7311a06826ad6b272a4c6155efc8ab0d1
Jack McNary <jack.mcnary@gmail.com>
2021-12-01 21:33:24 -0700

    updating belongsto and hasmany for index.js

--- models/index.js
+++ models/index.js
@@ -6,8 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -16,10 +15,13 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product'
-})
+  through: { model: ProductTag },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: { model: ProductTag },
+})
 
 module.exports = {
   Product,
