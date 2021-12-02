fd0ce8d765e454027e312f127f326cb1d9b3218b
Jack McNary <jack.mcnary@gmail.com>
2021-12-01 21:38:10 -0700

    filling out category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,33 +3,73 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
+      // find all categories
+      // be sure to include its associated Products
       include: [{ model: Category }],
     });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // find all categories
-  // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      // find all categories
+      // be sure to include its associated Products
+      include: [{ model: Category }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // find all categories
+      // be sure to include its associated Products
+      include: [{ model: Category }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // find all categories
+      // be sure to include its associated Products
+      include: [{ model: Category }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // find all categories
+      // be sure to include its associated Products
+      include: [{ model: Category }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete a category by its `id` value
 });
 
